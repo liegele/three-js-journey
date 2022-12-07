12e377490695fb78e75d476e4d4933ef95878616
liegele <liegele@gmail.com>
2022-12-07 15:35:03 -0300

    Criando pastas e arquivos iniciais.

--- 10-debug-ui/src/script.js
+++ 10-debug-ui/src/script.js
@@ -13,11 +13,19 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
+//Parameters
+const parameters = {
+  color: 0xff0000,
+  spin() {
+    gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + 10 });
+  },
+};
+
 /**
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({ color: parameters.color });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
@@ -93,14 +101,40 @@ tick();
 lil-gui debug interface
  */
 const gui = new GUI();
+
 gui.add(document, 'title');
 //position
-const folder1 = gui.addFolder('Position');
+const folder1 = gui.addFolder('Mesh Position');
 folder1.add(mesh.position, 'x', -3, 3, 0.1);
 folder1.add(mesh.position, 'y', -3, 3, 0.1);
 folder1.add(mesh.position, 'z', -3, 3, 0.1);
 //rotation
-const folder2 = gui.addFolder('Rotation');
+const folder2 = gui.addFolder('Mesh Rotation');
 folder2.add(mesh.rotation, 'x', -6, 6, 0.1);
 folder2.add(mesh.rotation, 'y', -6, 6, 0.1);
 folder2.add(mesh.rotation, 'z', -6, 6, 0.1);
+//Visibility
+const folder3 = gui.addFolder('Mesh Visibility');
+folder3.add(mesh, 'visible');
+//Wireframe
+const folder4 = gui.addFolder('Mesh Wireframe');
+folder4.add(material, 'wireframe');
+//Color
+const folder5 = gui.addFolder('Mesh Color');
+folder5.addColor(parameters, 'color').onChange(() => {
+  material.color.set(parameters.color);
+});
+//Spin button
+const folder6 = gui.addFolder('Mesh Spin');
+folder6.add(parameters, 'spin');
+
+//Hidding lil-gui panel
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'h') {
+    if (gui._hidden) {
+      gui.show();
+    } else {
+      gui.hide();
+    }
+  }
+});
