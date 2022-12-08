1d48b060b50edd1a903d6a3ae9008595dfde689e
liegele <liegele@gmail.com>
2022-12-08 00:26:47 -0300

    Atualizando arquivo

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -6,8 +6,31 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 Textures
 */
 
-const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load('/textures/door/color.jpg');
+const loadingManager = new THREE.LoadingManager();
+
+// loadingManager.onStart = () => console.log('onStart');
+// loadingManager.onProgress = () => console.log('onProgress');
+// loadingManager.onLoad = () => console.log('onLoad');
+// loadingManager.onError = () => console.log('onError');
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const colorTexture = textureLoader.load('/textures/door/color.jpg');
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg');
+const heightTexture = textureLoader.load('/textures/door/height.jpg');
+const normalTexture = textureLoader.load('/textures/door/normal.jpg');
+const ambientOcclusionTexture = textureLoader.load(
+  '/textures/door/ambientOcclusion.jpg'
+);
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg');
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg');
+
+// colorTexture.repeat.x = 2;
+// colorTexture.repeat.y = 3;
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping;
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping;
+
+colorTexture.offset.x = 0.5;
+colorTexture.offset.y = 0.5;
 
 /* const image = new Image();
 const texture = new THREE.Texture(image);
@@ -31,7 +54,7 @@ const scene = new THREE.Scene();
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ map: texture });
+const material = new THREE.MeshBasicMaterial({ map: colorTexture });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
